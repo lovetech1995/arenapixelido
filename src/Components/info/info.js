a248446e65d33ba83e8b42410346316de0c2428a
Tdev2023VFA <Tdev2023VFA@gmail.com>
2023-07-17 17:22:45 +0700

    upadte

--- src/Components/info/info.js
+++ src/Components/info/info.js
@@ -18,9 +18,9 @@ const Info = () => {
             <img src={title} className='absolute' alt="title" style={{ height: 234 * ratio, width: 1607 * ratio, top: 300 * ratio, left: 120 * ratio }} />
             <p className='absolute  text' style={{ fontSize: 36 * ratio, top: 700 * ratio, left: 200 * ratio }} >GameFi Tap to Earn NFT, with a completely unique gameplay and money-making method,<br />
                 promises to evoke a plethora of emotions for users</p>
-            <a><img src={webbuton} className='absolute' alt="webbuton" style={{ height: 83 * ratio, width: 83 * ratio, top: 850 * ratio, left: 760 * ratio }} /></a>
-            <a><img src={telegrambutton} className='absolute' alt="telegrambutton" style={{ height: 83 * ratio, width: 83 * ratio, top: 850 * ratio, left: 890 * ratio }} /></a>
-            <a><img src={twtbutton} className='absolute' alt="twtbutton" style={{ height: 83 * ratio, width: 83 * ratio, top: 850 * ratio, left: 1030 * ratio }} /></a>
+            <a href={'https://arenapixel.io/'}><img src={webbuton} className='absolute' alt="webbuton" style={{ height: 83 * ratio, width: 83 * ratio, top: 850 * ratio, left: 760 * ratio }} /></a>
+            <a href={'https://t.me/arenapixel'}><img src={telegrambutton} className='absolute' alt="telegrambutton" style={{ height: 83 * ratio, width: 83 * ratio, top: 850 * ratio, left: 890 * ratio }} /></a>
+            <a href={'https://twitter.com/ArenaPixel'}><img src={twtbutton} className='absolute' alt="twtbutton" style={{ height: 83 * ratio, width: 83 * ratio, top: 850 * ratio, left: 1030 * ratio }} /></a>
 
             <a href='#'><img src={marrketplace} className='absolute' alt="marrketplace" style={{ height: 100 * ratio, width: 359 * ratio, top: 900 * ratio, left: 150 * ratio }} /></a>
             <a href='#'><img src={pitchdeck} className='absolute' alt="pitchdeck" style={{ height: 100 * ratio, width: 359 * ratio, top: 870 * ratio, left: 1390 * ratio }} /></a>
